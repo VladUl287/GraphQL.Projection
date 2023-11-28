47ce3c5b7d38e6915de1964dad26d6b4753fe96b
ferry <ulyanovskiy.2001@mail.ru>
2023-11-28 18:52:51 +0300

    Create GraphQL.Projection.Tests

---
+++ GraphQL.Projection.Tests/Types/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace GraphQL.Projection.Tests.Types;
+
+public sealed class Order
+{
+    public long Id { get; init; }
+
+    public Address Address { get; init; } = default!;
+
+    public User User { get; init; } = default!;
+}
---
+++ GraphQL.Projection.Tests/Types/User.cs
@@ -0,0 +1,8 @@
+﻿namespace GraphQL.Projection.Tests.Types;
+
+public sealed class User
+{
+    public long Id { get; init; }
+
+    public string Name { get; init; } = string.Empty;
+}
