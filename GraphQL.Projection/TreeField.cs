98740ee42c1f7e247311995e808ab3a75bbb1431
ferry <ulyanovskiy.2001@mail.ru>
2023-12-07 18:57:51 +0300

    TreeField replace IEnumerable on Array

--- GraphQL.Projection/TreeField.cs
+++ GraphQL.Projection/TreeField.cs
@@ -1,8 +1,8 @@
 ﻿namespace GraphQL.Projection;
 
-public readonly struct TreeField(string name, IEnumerable<TreeField> children)
+public readonly struct TreeField(string name, TreeField[] children)
 {
     public required string Name { get; init; } = name ?? string.Empty;
 
-    public IEnumerable<TreeField> Children { get; init; } = children ?? Enumerable.Empty<TreeField>();
+    public TreeField[] Children { get; init; } = children ?? [];
 }
