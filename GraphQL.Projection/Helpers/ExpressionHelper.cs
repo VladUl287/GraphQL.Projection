3fd246eb211417a1274978876e15baee28983675
ferry <ulyanovskiy.2001@mail.ru>
2023-12-07 18:59:13 +0300

    Update expression helper

--- GraphQL.Projection/Helpers/ExpressionHelper.cs
+++ GraphQL.Projection/Helpers/ExpressionHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Collections;
 using System.Linq.Expressions;
 
 namespace GraphQL.Projection.Strategy.Helper;
@@ -12,7 +13,7 @@ public sealed class ExpressionHelper
         this.bindingContext = bindingContext;
     }
 
-    public Expression<Func<TEntity, TEntity>> GetLambdaExpression<TEntity>(IEnumerable<TreeField> fields)
+    public Expression<Func<TEntity, TEntity>> GetLambdaExpression<TEntity>(TreeField[] fields)
     {
         var parameter = Expression.Parameter(typeof(TEntity));
 
@@ -21,22 +22,126 @@ public sealed class ExpressionHelper
         return Expression.Lambda<Func<TEntity, TEntity>>(result, parameter);
     }
 
-    private MemberInitExpression GetMemberInitExpression(Type type, Expression parameter, IEnumerable<TreeField> fields)
+    private MemberInitExpression GetMemberInitExpression(Type type, Expression parameter, TreeField[] fields)
     {
-        var bindings = new List<MemberBinding>();
+        var stack = new Stack<MemberInitInfo>();
         
-        foreach (var field in fields)
+        stack.Push(new(type, parameter, fields, [], 0));
+
+        while (stack.Count > 0)
         {
-            var property = type.GetProperty(field.Name, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            var skipInitialization = false;
+            var info = stack.Peek();
 
-            if (property is not null)
+            for (var i = info.Index; i < info.Fields.Length; i++)
             {
-                var bind = bindingContext.Bind(property, parameter, field, GetMemberInitExpression);
+                var field = info.Fields[i];
 
-                bindings.Add(bind);
+                var property = info.Type.GetProperty(field.Name, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                if (property is null)
+                {
+                    continue;
+                }
+
+                var propertyType = property.PropertyType;
+
+                if (propertyType.IsPrimitive || propertyType.IsEnum || propertyType == typeof(string) || propertyType == typeof(Guid))
+                {
+                    var propertyParameter = Expression.Property(info.Parameter, property.Name);
+
+                    var bind = Expression.Bind(property, propertyParameter);
+
+                    info.Binds.Add(bind);
+                }
+                else if (propertyType.IsClass && propertyType.GetInterface(nameof(IEnumerable)) is null)
+                {
+                    var propertyParameter = Expression.Property(info.Parameter, property.Name);
+
+                    stack.Push(new(propertyType, propertyParameter, field.Children, [], 0, property));
+
+                    info.SetIndex(i + 1);
+
+                    skipInitialization = true;
+
+                    break;
+                }
+                else if (propertyType.IsGenericType)
+                {
+                    var elementType = propertyType.GenericTypeArguments[0];
+
+                    var propertyParameter = Expression.Parameter(elementType);
+
+                    stack.Push(new(elementType, propertyParameter, field.Children, [], 0, property));
+
+                    info.SetIndex(i + 1);
+
+                    skipInitialization = true;
+
+                    break;
+                }
+            }
+
+            if (skipInitialization)
+            {
+                continue;
+            }
+
+            if (stack.Count <= 1)
+            {
+                break;
+            }
+
+            var current = stack.Pop();
+            var previous = stack.Peek();
+
+            if (current.Property is not null)
+            {
+                var bind = bindingContext.Bind(current.Property, previous.Parameter, current.Parameter, current.Type, current.Binds);
+
+                previous.Binds.Add(bind);
             }
         }
 
-        return Expression.MemberInit(Expression.New(type), bindings);
+        var result = stack.Pop();
+
+        return Expression.MemberInit(Expression.New(result.Type), result.Binds);
+    }
+
+    private sealed class MemberInitInfo
+    {
+        public MemberInitInfo(Type type, Expression parameter, TreeField[] fields, List<MemberBinding> binds, int index, PropertyInfo? property = null)
+        {
+            Type = type ?? throw new ArgumentNullException();
+            Binds = binds ?? throw new ArgumentNullException();
+            Fields = fields ?? throw new ArgumentNullException();
+            Parameter = parameter ?? throw new ArgumentNullException();
+
+            Property = property;
+
+            SetIndex(index);
+        }
+
+        public Type Type { get; }
+
+        public Expression Parameter { get; }
+
+        public TreeField[] Fields { get; }
+
+        public List<MemberBinding> Binds { get; }
+
+        public PropertyInfo? Property { get; }
+
+        public int Index { get; private set; }
+
+        public void SetIndex(int index)
+        {
+            if (index < 0 || index > Fields?.Length)
+            {
+                throw new ArgumentException("Not correct index value");
+            }
+
+            Index = index;
+        }
     }
 }
