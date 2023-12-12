a2c2be39ba14373b7e3098e1077b00c037dd985e
ferry <ulyanovskiy.2001@mail.ru>
2023-12-12 20:15:40 +0300

    Create type extension

---
+++ GraphQL.Projection/Extensions/TypeExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphQL.Projection.Extensions;
+
+internal static class TypeExtensions
+{
+    public static bool IsPrimitive(this Type type)
+    {
+        ArgumentNullException.ThrowIfNull(type);
+
+        return type.IsPrimitive || type.IsEnum || type == typeof(string) || type == typeof(Guid);
+    }
+}
