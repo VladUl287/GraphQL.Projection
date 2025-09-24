d6ad53bfd9af65be688eec9bbe44fb8d92f44c27
User <ulyanovskiy.2001@mail.ru>
2025-09-24 13:33:53 +0300

    feat: implement query translator contract

--- GraphQL.Projection.Abstractions/Translation/IQueryTranslator.cs
+++ GraphQL.Projection.Abstractions/Translation/IQueryTranslator.cs
@@ -1,5 +1,10 @@
-﻿namespace GraphQL.Projection.Abstractions.Translation;
+﻿using GraphQLParser.AST;
+using System.Linq.Expressions;
 
-public interface IQueryTranslator
+namespace GraphQL.Projection.Abstractions.Translation;
+
+public interface IQueryTranslator<TEntity> where TEntity : class
 {
+    Expression<Func<TEntity, bool>> TranslateFilters(IEnumerable<ASTNode> filters);
+    IEnumerable<(LambdaExpression KeySelector, bool Descending)> TranslateSorts(IEnumerable<ASTNode> sorts);
 }
