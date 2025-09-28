9c8e0a01982c2eed8b1ee687339d2e6a853a330c
User <ulyanovskiy.2001@mail.ru>
2025-09-28 14:50:56 +0300

    feat: add console project

---
+++ GraphQLApi.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Projection.Models;
+using GraphQL.Projection.Pipeline;
+using GraphQLApi.Console;
+using GraphQLParser;
+
+var query = """
+    query {
+      search {
+        id
+      }
+    }
+    """;
+
+var pipeline = PipelineComposition.CreatePipeline<UserExt>();
+
+var document = Parser.Parse(query);
+
+var queryModel = pipeline(document, QueryModel<UserExt>.Empty);
+
+Console.WriteLine(queryModel.Select.ToString());
---
+++ GraphQLApi.Console/UserExt.cs
@@ -0,0 +1,9 @@
+﻿namespace GraphQLApi.Console;
+
+public sealed class UserExt
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public string Phone { get; init; }
+    public string Index { get; init; }
+}
