d804a7296d27e13a4ba57bc78eb3f0a09c6ddbac
User <ulyanovskiy.2001@mail.ru>
2025-09-30 14:33:00 +0300

    feat: add language ext functional lib for functional approach code ready solutions

--- GraphQL.Projection/Pipeline/SelectFeatureModule.cs
+++ GraphQL.Projection/Pipeline/SelectFeatureModule.cs
@@ -1,9 +1,10 @@
 ﻿using GraphQL.Projection.Extensions;
-using GraphQL.Projection.Helpers;
 using GraphQL.Projection.Models;
 using GraphQL.Projection.Resolvers;
-using GraphQL.Projection.Strategy.Binding;
 using GraphQLParser.AST;
+using LanguageExt;
+using LanguageExt.Common;
+using LanguageExt.Pipes;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -15,14 +16,8 @@ public static class SelectFeatureModule
     {
         return (document, model) =>
         {
-            var parameterResolver = new ParameterResolver();
-            var bindingContext = new BindingContext([new EntityStrategy(new Visitor.ParameterReplacerFabric()), new EnumerableStrategy(new ParameterResolver())]);
-            var typeResolver = new TypeResolver();
-            var fieldBinder = new FieldBuilder(new Fabrics.TypeBuilderFactory(typeResolver, bindingContext), typeResolver, bindingContext);
-            var typeBuilder = new TypeBuilder(fieldBinder);
-            var builder = new ExpressionBuilder(typeBuilder, new ParameterResolver());
-
             GraphQLSelectionSet? qLSelectionSet = null;
+            GraphQLField? qlField = null;
             foreach (var definition in document.Definitions)
             {
                 if (definition is { Kind: ASTNodeKind.OperationDefinition } and GraphQLOperationDefinition operation)
@@ -31,6 +26,7 @@ public static class SelectFeatureModule
                     {
                         if (selection is { Kind: ASTNodeKind.Field } and GraphQLField field)
                         {
+                            qlField = field;
                             qLSelectionSet = field.SelectionSet;
                         }
                     }
@@ -38,71 +34,152 @@ public static class SelectFeatureModule
                 }
             }
 
+            ArgumentNullException.ThrowIfNull(qlField);
             ArgumentNullException.ThrowIfNull(qLSelectionSet);
 
-            var parameter = Expression.Parameter(typeof(TEntity));
-
-            var assignements = BuildAssignements<TEntity>(parameter, qLSelectionSet);
-
-            var result = InitilizeMember<TEntity>(assignements);
-
-            var expression = ExpressionBuilder.BuildExpression<TEntity>(qLSelectionSet, (node) => typeBuilder.BuildType(typeof(TEntity), node),
-                (expression) => parameterResolver.GetParameterExpression(expression as MemberInitExpression));
+            var test = Build<TEntity>(qlField).Run();
+            var select = test.ThrowIfFail();
 
             return model with
             {
-                Select = Expression.Lambda<Func<TEntity, TEntity>>(result, parameter)
+                Select = select
             };
         };
     }
 
-    private static MemberInitExpression InitilizeMember<TEntity>(IEnumerable<MemberAssignment> assignments)
+    public static Eff<Expression<Func<TEntity, TEntity>>> Build<TEntity>(GraphQLField field)
     {
-        return Expression.MemberInit(Expression.New(typeof(TEntity)), assignments);
+        var result = Validate(field)
+            .SelectMany(validated => CreateParameter<TEntity>())
+            .SelectMany(parameter => BuildBindings<TEntity>(parameter, field))
+            .SelectMany(bindings => CreateMemberInit<TEntity>(bindings))
+            .SelectMany(memberInit => CreateLambda<TEntity>(memberInit));
+
+        return result;
     }
 
-    private static List<MemberAssignment> BuildAssignements<TEntity>(ParameterExpression parameter, GraphQLSelectionSet set)
+
+    private static Eff<Expression<Func<TEntity, TEntity>>> CreateLambda<TEntity>(MemberInitExpression memberInit)
     {
-        var bindings = new List<MemberAssignment>(set.Selections.Count);
-
-        AssignMember<TEntity>(parameter, bindings, set, 0);
-
-        return bindings;
-    }
-
-    private static void AssignMember<TEntity>(ParameterExpression parameter, List<MemberAssignment> result, GraphQLSelectionSet set, int index)
-    {
-        if (index >= set.Selections.Count)
-            return;
-
-        var member = set.Selections[index];
-        if (member is GraphQLField field)
+        return Prelude.Eff(() =>
         {
-            var assignement = Assign<TEntity>(parameter, field);
-            result.Add(assignement);
-            return;
-        }
-
-        AssignMember<TEntity>(parameter, result, set, index + 1);
+            var parameter = new ParameterResolver().GetParameterExpression(memberInit);
+            return Expression.Lambda<Func<TEntity, TEntity>>(memberInit, parameter);
+        });
     }
 
-    private static MemberAssignment Assign<TEntity>(ParameterExpression parameter, GraphQLField field)
+    private static Eff<GraphQLField> Validate(GraphQLField field)
     {
-        var fieldName = field.Name.StringValue;
-        var property = typeof(TEntity).GetProperty(fieldName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
-
-        ArgumentNullException.ThrowIfNull(property);
-
-        var propType = property.PropertyType;
-
-        if (propType.IsPrimitive())
-        {
-            var memberAccess = Expression.MakeMemberAccess(parameter, property);
-            return Expression.Bind(property, memberAccess);
-        }
-
-        var assignements = BuildAssignements<TEntity>(parameter, field.SelectionSet);
-        var memberInit = InitilizeMember<TEntity>(assignements);
-        return Expression.Bind(property, memberInit);
+        return Prelude
+            .EffMaybe(() => field is not null ? Fin<GraphQLField>.Succ(field) : Fin<GraphQLField>.Fail(Error.New("Field is null")))
+            .SelectMany((notNull) =>
+            {
+                return notNull.Name.IsNull() ?
+                    Prelude.FailEff<GraphQLField>("Field name cannot be empty") :
+                    Prelude.SuccessEff(notNull);
+            });
     }
+
+    private static Eff<Seq<MemberBinding>> BuildBindings<TEntity>(ParameterExpression parameter, GraphQLField field)
+    {
+        var fields = field.SelectionSet.Selections.Where(c => c is GraphQLField).Select(c => c as GraphQLField).ToArray();
+        return fields
+            .Map(selection => CreateBinding<TEntity>(parameter, selection))
+            .Sequence()
+            .SelectMany(bindings => bindings.IsDefault()
+                ? Prelude.FailEff<Seq<MemberBinding>>("No valid bindings created")
+                : Prelude.SuccessEff<Seq<MemberBinding>>(bindings.ToSeq()));
+    }
+
+    private static Eff<MemberBinding> CreateBinding<TEntity>(
+        ParameterExpression parameter,
+        GraphQLField selection)
+    {
+        return FindProperty<TEntity>(selection.Name.StringValue)
+            .SelectMany(property => CreatePropertyBinding(parameter, selection, property));
+    }
+
+    private static Eff<PropertyInfo> FindProperty<TEntity>(string propertyName)
+    {
+        var property = typeof(TEntity).GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+        return Eff<PropertyInfo>.Success(property)
+            .SelectMany(prop => prop is null
+                ? Prelude.FailEff<PropertyInfo>($"Property '{propertyName}' not found on User")
+                : Prelude.SuccessEff(prop));
+    }
+
+    private static Eff<MemberBinding> CreatePropertyBinding(
+        ParameterExpression parameter,
+        GraphQLField selection,
+        PropertyInfo property)
+    {
+        var type = property.PropertyType.IsPrimitive()
+            ? CreateSimplePropertyAccess(parameter, property)
+            : CreateNestedExpression(parameter, selection, property);
+
+        return type.SelectMany(expression => Prelude.Eff(() => (MemberBinding)Expression.Bind(property, expression)));
+    }
+
+    private static Eff<Expression> CreateSimplePropertyAccess(ParameterExpression parameter, PropertyInfo property) =>
+        Prelude.Eff(() => (Expression)Expression.MakeMemberAccess(parameter, property));
+
+    private static Eff<Expression> CreateNestedExpression(ParameterExpression parameter, GraphQLField selection, PropertyInfo property)
+    {
+        return Prelude.Eff(() => Expression.MakeMemberAccess(parameter, property))
+            .SelectMany(propertyAccess =>
+                Prelude.Eff(() => property.PropertyType)
+                    .SelectMany(nestedType =>
+                        BuildNestedSelector(propertyAccess, selection, nestedType)));
+    }
+
+    private static Eff<Expression> BuildNestedSelector(
+        Expression propertyAccess,
+        GraphQLField field,
+        Type nestedType)
+    {
+        return field.SelectionSet.Selections.Where(c => c is GraphQLField).Select(c => c as GraphQLField)
+            .Map(selection => CreateNestedBinding(propertyAccess, selection, nestedType))
+            .Sequence()
+            .SelectMany(bindings =>
+                Prelude.Eff(() => nestedType.GetConstructors().First())
+                    .SelectMany(constructor =>
+                        Prelude.Eff(() => Expression.New(constructor))
+                            .SelectMany(newExpr =>
+                                Prelude.Eff(() => (Expression)Expression.MemberInit(newExpr, bindings)))));
+    }
+
+    private static Eff<MemberBinding> CreateNestedBinding(
+            Expression parentAccess,
+            GraphQLField selection,
+            Type parentType)
+    {
+        return Prelude.Eff(() => parentType.GetProperty(selection.Name.StringValue))
+            .SelectMany(prop => prop is null
+                ? Prelude.FailEff<PropertyInfo>($"Property '{selection.Name}' not found on {parentType.Name}")
+                : Prelude.SuccessEff(prop))
+            .SelectMany(property =>
+            {
+                var prop = selection.SelectionSet.Selections.Count == 0
+                    ? Prelude.Eff(() => (Expression)Expression.Property(parentAccess, property))
+                    : BuildNestedSelector(Expression.Property(parentAccess, property), selection, property.PropertyType);
+                return prop.SelectMany(expression => Prelude.Eff(() => (MemberBinding)Expression.Bind(property, expression)));
+            });
+    }
+
+    private static Eff<MemberInitExpression> CreateMemberInit<TEntity>(Seq<MemberBinding> bindings)
+    {
+        return Eff<ConstructorInfo>.Success(typeof(TEntity).GetConstructors().FirstOrDefault())
+            .SelectMany(constructor =>
+            {
+                if (constructor is null)
+                {
+                    return Prelude.FailEff<ConstructorInfo>("No constructor found for object type");
+                }
+                return Prelude.SuccessEff(constructor);
+            })
+            .SelectMany(constructor => Eff<NewExpression>.Success(Expression.New(constructor)))
+            .SelectMany(newExpr => Eff<MemberInitExpression>.Success(Expression.MemberInit(newExpr, bindings)));
+    }
+
+    private static Eff<ParameterExpression> CreateParameter<TEntity>() => Prelude.Eff(() => Expression.Parameter(typeof(TEntity)));
 }
