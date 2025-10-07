baa7455d97ab29e7f2de9ca4a9e2370c34d197b1
User <ulyanovskiy.2001@mail.ru>
2025-10-07 11:47:43 +0300

    feat: replce func from generic to type parameters and add sub entities support

--- GraphQL.Projection/Pipeline/SelectFeatureModule.cs
+++ GraphQL.Projection/Pipeline/SelectFeatureModule.cs
@@ -13,8 +13,8 @@ public static class SelectFeatureModule
         return (selectionSet, model) =>
         {
             var parameter = Expression.Parameter(typeof(TEntity));
-            var bindings = BuildAssignements<TEntity>(parameter, selectionSet);
-            var result = MemberInit<TEntity>(bindings);
+            var bindings = BuildAssignements(typeof(TEntity), parameter, selectionSet);
+            var result = MemberInit(typeof(TEntity), bindings);
             var expression = Expression.Lambda<Func<TEntity, TEntity>>(result, parameter);
             return model with
             {
@@ -23,21 +23,21 @@ public static class SelectFeatureModule
         };
     }
 
-    private static MemberInitExpression MemberInit<TEntity>(IEnumerable<MemberAssignment> assignments)
+    private static MemberInitExpression MemberInit(Type type, IEnumerable<MemberAssignment> assignments)
     {
-        return Expression.MemberInit(Expression.New(typeof(TEntity)), assignments);
+        return Expression.MemberInit(Expression.New(type), assignments);
     }
 
-    private static List<MemberAssignment> BuildAssignements<TEntity>(ParameterExpression parameter, GraphQLSelectionSet set)
+    private static List<MemberAssignment> BuildAssignements(Type type, Expression parameter, GraphQLSelectionSet set)
     {
         var bindings = new List<MemberAssignment>(set.Selections.Count);
 
-        AssignMember<TEntity>(parameter, bindings, set, 0);
+        AssignMember(type, parameter, bindings, set, 0);
 
         return bindings;
     }
 
-    private static void AssignMember<TEntity>(ParameterExpression parameter, List<MemberAssignment> result, GraphQLSelectionSet set, int index)
+    private static void AssignMember(Type type, Expression parameter, List<MemberAssignment> result, GraphQLSelectionSet set, int index)
     {
         if (index >= set.Selections.Count)
             return;
@@ -45,18 +45,17 @@ public static class SelectFeatureModule
         var member = set.Selections[index];
         if (member is GraphQLField field)
         {
-            var assignement = Assign<TEntity>(parameter, field);
+            var assignement = Assign(type, parameter, field);
             result.Add(assignement);
-            return;
         }
 
-        AssignMember<TEntity>(parameter, result, set, index + 1);
+        AssignMember(type, parameter, result, set, index + 1);
     }
 
-    private static MemberAssignment Assign<TEntity>(ParameterExpression parameter, GraphQLField field)
+    private static MemberAssignment Assign(Type type, Expression parameter, GraphQLField field)
     {
         var fieldName = field.Name.StringValue;
-        var property = typeof(TEntity).GetProperty(fieldName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+        var property = type.GetProperty(fieldName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
 
         ArgumentNullException.ThrowIfNull(property);
 
@@ -68,8 +67,9 @@ public static class SelectFeatureModule
             return Expression.Bind(property, memberAccess);
         }
 
-        var assignements = BuildAssignements<TEntity>(parameter, field.SelectionSet);
-        var memberInit = MemberInit<TEntity>(assignements);
+        var subParameter = Expression.Property(parameter, property);
+        var assignements = BuildAssignements(propType, subParameter, field.SelectionSet);
+        var memberInit = MemberInit(propType, assignements);
         return Expression.Bind(property, memberInit);
     }
 }
