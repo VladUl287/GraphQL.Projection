26cd0048398c791966a651f6b7bc93bc5fad6a55
User <ulyanovskiy.2001@mail.ru>
2025-10-07 11:48:29 +0300

    feat: add type extensions additional funcs for collections support

--- GraphQL.Projection/Extensions/TypeExtensions.cs
+++ GraphQL.Projection/Extensions/TypeExtensions.cs
@@ -1,11 +1,27 @@
-﻿namespace GraphQL.Projection.Extensions;
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace GraphQL.Projection.Extensions;
 
 internal static class TypeExtensions
 {
-    internal static bool IsPrimitive(this Type type)
-    {
-        ArgumentNullException.ThrowIfNull(type);
+    internal static bool IsPrimitive(this Type type) => type.IsPrimitive || type.IsEnum || type == typeof(string) || type == typeof(Guid);
+    internal static bool IsCollection(this Type type) => type.ImplementsGenericInterface(typeof(ICollection<>));
+    internal static bool IsEnumerable(this Type type) => type.IsAssignableTo(typeof(IEnumerable));
 
-        return type.IsPrimitive || type.IsEnum || type == typeof(string) || type == typeof(Guid);
+    internal static bool ImplementsGenericInterface(this Type type, Type interfaceType)
+    {
+        if (type.IsGenericType(interfaceType))
+            return true;
+
+        foreach (var inter in type.GetInterfaces().AsSpan())
+            if (inter.IsGenericType(interfaceType))
+                return true;
+
+        return false;
     }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static bool IsGenericType(this Type type, Type genericType) =>
+        type.IsGenericType && type.GetGenericTypeDefinition() == genericType;
 }
