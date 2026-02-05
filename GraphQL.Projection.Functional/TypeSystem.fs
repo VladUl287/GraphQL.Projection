4bc77201278ba6c6e8410a86c0e38718036f2adc
User <ulyanovskiy.2001@mail.ru>
2026-02-05 13:12:07 +0300

    feat: create type system for better extensibility and testibility

---
+++ GraphQL.Projection.Functional/TypeSystem.fs
@@ -0,0 +1,51 @@
+﻿module TypeSystem
+    open System
+    open System.Collections
+    open System.Collections.Generic
+    
+    type TypeInspector = {
+        IsPrimitive: Type -> bool
+        IsCollection: Type -> bool
+        IsSubtypeOf: Type -> string -> bool
+        GetElementType: Type -> Type option
+    }
+
+    let defaultInspector: TypeInspector =
+        let rec isSubtypeOf (typ: Type) (typeName: string): bool =
+            typ <> null && (
+                typ.Name = typeName || isSubtypeOf typ.BaseType typeName ||
+                typ.GetInterfaces() 
+                    |> Array.exists (fun iface -> isSubtypeOf iface typeName)
+            )
+
+        let rec isPrimitive (typ: Type): bool = 
+            typ.IsPrimitive || typ.IsEnum || typ = typeof<string> || typ = typeof<Guid> ||
+            typ = typeof<DateTime> || typ = typeof<DateTimeOffset> || typ = typeof<TimeSpan> || 
+            typ = typeof<DateOnly> || typ = typeof<TimeOnly>  || typ = typeof<decimal> || (
+                typ.IsGenericType && typ.GetGenericTypeDefinition() = typeof<Nullable<_>> && 
+                typ.GetGenericArguments() 
+                    |> Array.tryItem 0 
+                    |> Option.exists isPrimitive 
+            )
+
+        let isCollection (typ: Type): bool = typ.IsAssignableTo(typeof<IEnumerable>)
+
+        let rec getElementType (typ: Type): Type option =
+            match typ with
+            | t when t.IsArray -> Some (t.GetElementType())
+            | t when t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<IEnumerable<_>> -> 
+                typ.GetGenericArguments() 
+                    |> Array.tryItem 0
+            | t when typ.IsGenericType && typ.GetGenericTypeDefinition() = typeof<Nullable<_>> ->
+                typ.GetGenericArguments() 
+                    |> Array.tryPick getElementType
+            | t ->
+                t.GetInterfaces() 
+                    |> Array.tryPick getElementType
+
+        {
+            IsPrimitive = isPrimitive
+            IsSubtypeOf = isSubtypeOf
+            IsCollection = isCollection
+            GetElementType = getElementType
+        }
