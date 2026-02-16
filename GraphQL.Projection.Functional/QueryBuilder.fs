6849af0b6fc60cc8b1eaa330a7cf0e37510a51c2
User <ulyanovskiy.2001@mail.ru>
2026-02-16 17:20:45 +0300

    feat: pass factory through context

--- GraphQL.Projection.Functional/QueryBuilder.fs
+++ GraphQL.Projection.Functional/QueryBuilder.fs
@@ -7,14 +7,15 @@ open ExpressionSystem
 
 type QueryContext<'a> = {
     GraphQL: GraphQLOperations
+    QueryFactory: BuilderFactory<'a>
 }
 
 let project<'a> (ctx: QueryContext<'a>) (ast: GraphQLOp<GraphQLNode>) (query: IQueryable<'a>): IQueryable<obj> =
-    let { GraphQL = graph; } = ctx
+    let { GraphQL = graph; QueryFactory = factory } = ctx
 
     let normalized = graph.Normilize ast
     let node = graph.Interpret normalized
 
-    let builder = createFactory<'a> node
+    let builder = factory.Create node
 
     builder.Invoke(query)
