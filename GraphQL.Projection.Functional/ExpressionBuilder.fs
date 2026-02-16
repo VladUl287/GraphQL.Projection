8d5663c2726247b5435f97f3b717dd16a2a87fca
User <ulyanovskiy.2001@mail.ru>
2026-02-16 12:09:55 +0300

    feat: use projection high level

--- GraphQL.Projection.Functional/ExpressionBuilder.fs
+++ GraphQL.Projection.Functional/ExpressionBuilder.fs
@@ -14,8 +14,25 @@ let buildSelect<'a> (node: GraphQLNode): Expression<Func<'a, obj>> =
     let select = fun (arg: 'a) -> arg :> obj
     select
 
-type QueryExpressionBuilder<'a> = {
-    BuildSelect: GraphQLNode -> Expression<Func<'a, obj>>
-    BuildWhere: GraphQLNode -> Expression<Func<'a, bool>>
-    BuildOrderBy: GraphQLNode -> Expression<Func<'a, obj>>
+type SortDirection = Ascending | Descending
+
+type OrderByOperation<'a> = {
+    Expression: Expression<Func<'a, obj>>
+    Direction: SortDirection
+}
+
+//type PaginationOperation<'a> = {
+//    Take: int
+//    Skip: int option
+//    After: obj option
+//}
+
+type QueryOperations<'a> = {
+    Select: GraphQLNode -> Expression<Func<'a, obj>>
+    Where: GraphQLNode -> Expression<Func<'a, bool>>
+    OrderBy: GraphQLNode -> OrderByOperation<'a>
+
+    //Pagination : PaginationOperation option
+    //Distinct : bool
+    //GroupBy : Expression<Func<'a, obj>> option
 }
--- GraphQL.Projection.Functional/QueryBuilder.fs
+++ GraphQL.Projection.Functional/QueryBuilder.fs
@@ -7,12 +7,17 @@ open GraphQLOp
 
 type QueryContext<'a> = {
     GraphQL: GraphQLOperations
-    Expressions: QueryExpressionBuilder<'a>
+    Query: QueryOperations<'a>
 }
 
 let project<'a> (ctx: QueryContext<'a>) (ast: GraphQLOp<GraphQLNode>) (query: IQueryable<'a>): IQueryable<obj> =
     let normalized = ctx.GraphQL.Normilize ast
     let node = ctx.GraphQL.Interpret normalized
 
-    let selectExp = ctx.Expressions.BuildSelect node
-    query.Select(selectExp)
+    let selectExp = ctx.Query.Select node
+    let whereExpr = ctx.Query.Where node
+    let orderByExpr = ctx.Query.OrderBy node
+    query
+        .Where(whereExpr)
+        .OrderBy(orderByExpr.Expression)
+        .Select(selectExp)
